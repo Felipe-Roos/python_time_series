021cc441fd2c6c95599ac38fc77c8626c46d2780
Felipe-Roos <feliperoos941@gmail.com>
2025-08-25 20:33:46 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,7 @@
+"O que é um cabeçalho de dentro de um arquivo python e para que server o que é um Docstring, e para que server"
+""
+"O docString ele serve como algo para identificar e explicar de forma rapido o intuitiva o código, é usado exclusivamente para descrever um código especifico"
 
+"O cabeçalho ele trablha executando alguns scrips e para executar o arquivo e realizando uma conversão de dados"
+
+"Enquanto um atua descrevendo o que o código fonte vai fazer, 1 atua fazendo com o que o código rode"
