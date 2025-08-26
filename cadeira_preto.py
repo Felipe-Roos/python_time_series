dd5f1520661c4de2d8b74c91270ba56f62b3ad83
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-26 14:11:08 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -5,3 +5,28 @@
 "O cabeçalho ele trablha executando alguns scrips e para executar o arquivo e realizando uma conversão de dados"
 
 "Enquanto um atua descrevendo o que o código fonte vai fazer, 1 atua fazendo com o que o código rode"
+
+# atividade 1 = formete o cabeçalho deste arquivo
+
+# atividade 2 = implemente as funções abaixo e coloque as docstrings
+
+
+
+def maximo(nums):
+    """oque faz
+    oque recebe
+    oque retorna"""
+    # TODO: percorra a lista guardando o maior atual
+    ...
+
+
+def e_par(n: int) -> bool:
+    """ ... """
+    # TODO: retorne se n é par
+    ...
+  
+
+def fatorial(n: int) -> int:
+    """   ...  """
+    # TODO: implemente de forma iterativa (sem recursão)
+    ...
